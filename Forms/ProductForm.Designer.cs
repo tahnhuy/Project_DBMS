e25f228d4e76a3d726d96568b865f71002ce3c5c
Nhat Huy <tahnhuy25@gmail.com>
2025-09-17 08:16:11 +0700

    chuc nang tim kiem san pham theo ten va id

--- Forms/ProductForm.Designer.cs
+++ Forms/ProductForm.Designer.cs
@@ -29,34 +29,85 @@
         private void InitializeComponent()
         {
             this.dgv_Products = new System.Windows.Forms.DataGridView();
+            this.txt_nameSearch = new System.Windows.Forms.TextBox();
+            this.lbl_nameSearch = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_idSearch = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Products)).BeginInit();
             this.SuspendLayout();
             // 
             // dgv_Products
             // 
             this.dgv_Products.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_Products.Location = new System.Drawing.Point(21, 246);
+            this.dgv_Products.Location = new System.Drawing.Point(21, 206);
             this.dgv_Products.Name = "dgv_Products";
             this.dgv_Products.RowHeadersWidth = 82;
             this.dgv_Products.RowTemplate.Height = 33;
-            this.dgv_Products.Size = new System.Drawing.Size(1313, 565);
+            this.dgv_Products.Size = new System.Drawing.Size(1313, 605);
             this.dgv_Products.TabIndex = 0;
+            // 
+            // txt_nameSearch
+            // 
+            this.txt_nameSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nameSearch.Location = new System.Drawing.Point(300, 49);
+            this.txt_nameSearch.Name = "txt_nameSearch";
+            this.txt_nameSearch.Size = new System.Drawing.Size(300, 44);
+            this.txt_nameSearch.TabIndex = 1;
+            this.txt_nameSearch.TextChanged += new System.EventHandler(this.txt_SearchProduct_TextChanged);
+            // 
+            // lbl_nameSearch
+            // 
+            this.lbl_nameSearch.AutoSize = true;
+            this.lbl_nameSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl_nameSearch.Location = new System.Drawing.Point(25, 55);
+            this.lbl_nameSearch.Name = "lbl_nameSearch";
+            this.lbl_nameSearch.Size = new System.Drawing.Size(257, 31);
+            this.lbl_nameSearch.TabIndex = 2;
+            this.lbl_nameSearch.Text = "Tìm sản phẩm(Tên):";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(25, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tìm sản phẩm(ID):";
+            // 
+            // txt_idSearch
+            // 
+            this.txt_idSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_idSearch.Location = new System.Drawing.Point(300, 120);
+            this.txt_idSearch.Name = "txt_idSearch";
+            this.txt_idSearch.Size = new System.Drawing.Size(300, 44);
+            this.txt_idSearch.TabIndex = 4;
+            this.txt_idSearch.TextChanged += new System.EventHandler(this.txt_idSearch_TextChanged);
             // 
             // ProductForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1346, 823);
+            this.Controls.Add(this.txt_idSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_nameSearch);
+            this.Controls.Add(this.txt_nameSearch);
             this.Controls.Add(this.dgv_Products);
             this.Name = "ProductForm";
-            this.Text = "ProductForm";
+            this.Text = "Quản lý sản phẩm";
             ((System.ComponentModel.ISupportInitialize)(this.dgv_Products)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgv_Products;
+        private System.Windows.Forms.TextBox txt_nameSearch;
+        private System.Windows.Forms.Label lbl_nameSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_idSearch;
     }
 }
--- Forms/ProductForm.cs
+++ Forms/ProductForm.cs
@@ -13,9 +13,12 @@ namespace Sale_Management.Forms
 {
     public partial class ProductForm : Form
     {
+        private ProductRepository productRepository;
+        
         public ProductForm()
         {
             InitializeComponent();
+            productRepository = new ProductRepository();
             LoadProducts();
         }
 
@@ -25,19 +28,90 @@ namespace Sale_Management.Forms
             {
                 DataTable dt = DatabaseConnection.ExecuteQuery("GetAllProducts", CommandType.StoredProcedure, null);
                 dgv_Products.DataSource = dt;
-
-                if (dgv_Products.Columns.Count > 0)
-                {
-                    dgv_Products.Columns["ProductID"].HeaderText = "Mã sản phẩm";
-                    dgv_Products.Columns["ProductName"].HeaderText = "Tên sản phẩm";
-                    dgv_Products.Columns["StockQuantity"].HeaderText = "Số lượng tồn";
-                    dgv_Products.Columns["Price"].HeaderText = "Giá";
-                    dgv_Products.Columns["Unit"].HeaderText = "Đơn vị";
-                }
+                ConfigureDataGridView();
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Lỗi tải dữ liệu của hàng hóa: " + ex.Message);
+            }
+        }
+        
+        private void ConfigureDataGridView()
+        {
+            if (dgv_Products.Columns.Count > 0)
+            {
+                dgv_Products.Columns["ProductID"].HeaderText = "Mã sản phẩm";
+                dgv_Products.Columns["ProductName"].HeaderText = "Tên sản phẩm";
+                dgv_Products.Columns["StockQuantity"].HeaderText = "Số lượng tồn";
+                dgv_Products.Columns["Price"].HeaderText = "Giá";
+                dgv_Products.Columns["Unit"].HeaderText = "Đơn vị";
+            }
+        }
+        
+        //  Tim kiem san pham theo ten bang function 
+        private void SearchProductsByName(string searchText)
+        {
+            try
+            {
+                DataTable dt;
+                if (string.IsNullOrWhiteSpace(searchText))
+                { 
+                    dt = DatabaseConnection.ExecuteQuery("GetAllProducts", CommandType.StoredProcedure, null);
+                }
+                else
+                {
+                    dt = productRepository.GetProductByName(searchText);
+                }
+                
+                dgv_Products.DataSource = dt;
+                ConfigureDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tìm kiếm sản phẩm: " + ex.Message);
+            }
+        }
+        
+        private void txt_SearchProduct_TextChanged(object sender, EventArgs e)
+        {
+            SearchProductsByName(txt_nameSearch.Text.Trim());
+
+            if (txt_nameSearch.Focused)
+            {
+                txt_idSearch.Text = string.Empty;
+            }
+        }
+
+        // Tìm kiếm sản phẩm theo ID bằng function
+        private void SearchProductsByID(string searchText)
+        {
+            try
+            {
+                DataTable dt;
+                if (string.IsNullOrWhiteSpace(searchText))
+                {
+                    dt = DatabaseConnection.ExecuteQuery("GetAllProducts", CommandType.StoredProcedure, null);
+                }
+                else
+                {
+                    dt = productRepository.GetProductById(int.Parse(searchText));
+                }
+
+                dgv_Products.DataSource = dt;
+                ConfigureDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi tìm kiếm sản phẩm: " + ex.Message);
+            }
+        }
+        private void txt_idSearch_TextChanged(object sender, EventArgs e)
+        {
+            SearchProductsByID(txt_idSearch.Text);
+
+            if (txt_idSearch.Focused)
+            {
+                txt_nameSearch.Text = string.Empty;
             }
         }
     }
